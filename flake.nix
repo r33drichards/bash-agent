173977b101ac9625b55243f5df8ce3d262bf122d
r33drichards <rwendt1337@gmail.com>
2025-07-13 21:21:10 -0700

    docker image

--- flake.nix
+++ flake.nix
@@ -168,6 +168,7 @@
           config = {
             Entrypoint = [ "${bashAgentEntrypoint}" ];
             WorkingDir = "/app";
+            User = "1000:1000";
             Env = [
               "PYTHONUNBUFFERED=1"
               "LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.glibc ]}"
@@ -188,6 +189,7 @@
           config = {
             Entrypoint = [ "${agentEntrypoint}" ];
             WorkingDir = "/app";
+            User = "1000:1000";
             Env = [
               "PYTHONUNBUFFERED=1"
               "LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib pkgs.glibc ]}"
