49cef13034669629cb3278645b12ffa84ba4ec8f
r33drichards <rwendt1337@gmail.com>
2025-08-24 20:16:52 -0700

    commands

--- flake.nix
+++ flake.nix
@@ -160,10 +160,10 @@
           tag = "latest";
           maxLayers = 120;
           contents = baseContents;
-          extraCommands = [
-            "mkdir -p /opt/google/chrome"
-            "ln -s ${lib.getExe pkgs.google-chrome} /opt/google/chrome/chrome"
-          ];
+          extraCommands = ''
+            mkdir -p /opt/google/chrome
+            ln -s ${lib.getExe pkgs.google-chrome} /opt/google/chrome/chrome
+          '';
           config = {
             Entrypoint = [ "${lib.getExe webAgentExecutable}" ];
             WorkingDir = "/app";
